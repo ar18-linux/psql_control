bbd854c9379336676d13bb3c8824c666d6496f30
nulysses <adr.zivil@gmail.com>
2021-04-28 08:53:41 +0200

    half-way through adapting

--- prepare_import_server.sh
+++ prepare_import_server.sh
@@ -1,20 +1,59 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-psql_dir="$(cat "${DIR}/vars/psql_dir")"
-ram_db_dir="$(cat "${DIR}/vars/ram_db_dir")"
-_7za="$(cat "${DIR}/vars/7z_exec")"
+set -e
+set -x
 
-ram_db="${ram_db_dir}/7777"
-pg_ctl="${psql_dir}/bin/pg_ctl"
-init_db="${psql_dir}/bin/initdb"
 
-"${pg_ctl}" -D "${ram_db}" stop  -m f
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-rm -rf "${ram_db}"
 
-mkdir "${ram_db}"
+function init_vars(){
+  echo init_vars
+  . "${script_dir}/vars"
+}
 
-"${init_db}" -D "${ram_db}" -E utf-8
-sed -i 's/#port = 5432/port = 7777/g' "${ram_db}/postgresql.conf"
-"${pg_ctl}" -D "${ram_db}" start
+
+function sanity_check(){
+  echo sanity_check
+  if [[ "$(whoami)" != "root" ]]; then
+    read -p "not root"
+    exit 1
+  fi
+  echo "_7za: ${_7za}"
+  echo "psql_dir: ${psql_dir}"
+  echo "ram_db_dir: ${ram_db_dir}"
+  echo "db_user: ${db_user}"
+}
+
+
+function prepare(){
+  ram_db="${ram_db_dir}/7777"
+  pg_ctl="${psql_dir}/bin/pg_ctl"
+  init_db="${psql_dir}/bin/initdb"
+  
+  if [[ -d "${ram_db}" ]]; then
+    set +e
+    su - "${db_user}" -c "${pg_ctl} -D ${ram_db} stop  -m f"
+    set -e
+  fi
+  
+  rm -rf "${ram_db}"
+  
+  mkdir -p "${ram_db}"
+  chmod 0700 "${ram_db}"
+  chown "${db_user}" "${ram_db}"
+  
+  su - "${db_user}" -c "${init_db} -D ${ram_db} -E utf-8"
+  sed -i 's/#port = 5432/port = 7777/g' "${ram_db}/postgresql.conf"
+  su - "${db_user}" -c "${pg_ctl} -D ${ram_db} start"
+}
+
+
+function run(){
+  init_vars
+  sanity_check
+  prepare
+}
+
+
+run
--- restore_ram_db.sh
+++ restore_ram_db.sh
@@ -1,45 +1,83 @@
 #!/bin/bash
 
+set -e
+set -x
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-psql_dir="$(cat "${DIR}/vars/psql_dir")"
-ram_db_dir="$(cat "${DIR}/vars/ram_db_dir")"
-target="$(cat "${DIR}/vars/zip_dir")"
-_7za="$(cat "${DIR}/vars/7z_exec")"
-pg_ctl="${psql_dir}/bin/pg_ctl"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-str="$(grep '[[:alnum:]]' "${DIR}/dbs.txt" | tail -n 1 | xargs)"
-stringarray=($str)
-source="${stringarray[0]}"
-port="${stringarray[1]}"
-sub_dir="default"
 
-if [ -n "$port" ]; then
-  sub_dir="${port}"
-fi
+function init_vars(){
+  echo init_vars
+  . "${script_dir}/vars"
+}
 
-ram_db="${ram_db_dir}/${sub_dir}"
 
-read -p "restore ${str}?"
+function sanity_check(){
+  echo sanity_check
+  if [[ "$(whoami)" != "root" ]]; then
+    read -p "not root"
+    exit 1
+  fi
+  echo "_7za: ${_7za}"
+  echo "psql_dir: ${psql_dir}"
+  echo "ram_db_dir: ${ram_db_dir}"
+  echo "target: ${target}"
+  echo "db_user: ${db_user}"
+}
 
-pid=$(head -1 "${ram_db}/postmaster.pid")
-#taskkill //F //PID $pid || /bin/kill -INT $pid || "${pg_ctl}" -D "${ram_db}" stop
-"${pg_ctl}" -D "${ram_db}" stop -m f
-#echo waiting 10 secs...
-#sleep 10
 
-rm -rf "${ram_db}"
+function restore(){
+  pg_ctl="${psql_dir}/bin/pg_ctl"
+  str="$(grep '[[:alnum:]]' "${script_dir}/dbs.txt" | tail -n 1 | xargs)"
+  stringarray=($str)
+  source="${stringarray[0]}"
+  port="${stringarray[1]}"
+  sub_dir="default"
+  
+  if [ -n "$port" ]; then
+    sub_dir="${port}"
+  fi
+  
+  ram_db="${ram_db_dir}/${sub_dir}"
+  
+  read -p "restore ${str}?"
+  
+  #pid=$(head -1 "${ram_db}/postmaster.pid")
+  #taskkill //F //PID $pid || /bin/kill -INT $pid || "${pg_ctl}" -D "${ram_db}" stop
+  set +e
+  su - "${db_user}" -c "${pg_ctl} -D ${ram_db} stop -m f"
+  set -e
+  #echo waiting 10 secs...
+  #sleep 10
+  
+  rm -rf "${ram_db}"
+  
+  mkdir "${ram_db}"
+  chown "${db_user}" "${ram_db}"
+  
+  su - "${db_user}" -c "${_7za} x -bsp1 ${target}/${source}.7z -aoa -o${ram_db}" 
+  
+  if [ -n "$port" ]; then
+    sed -i -E "s/#?port = .+/port = ${port}/" "${ram_db}/postgresql.conf"
+    #chown "${db_user}" "${ram_db}/postgresql.conf"
+  fi
+  
+  chmod 0700 "${ram_db}"
+  chown "${db_user}" "${ram_db}"
+  
+  echo "restored ${source}"
+  echo "Press a key"
+  su - "${db_user}" -c "${pg_ctl} -D ${ram_db} start"
+  #nohup su - "${db_user}" -c "${pg_ctl} -D ${ram_db} start" &
+}
 
-mkdir "${ram_db}"
 
-"${_7za}" x -bsp1 "${target}/${source}.7z" -aoa -o"${ram_db}" 
+function run(){
+  init_vars
+  sanity_check
+  restore
+}
 
-if [ -n "$port" ]; then
-  sed -i -E "s/#?port = .+/port = ${port}/" "${ram_db}/postgresql.conf"
-fi
 
-chmod 0700 "${ram_db}"
+run
 
-echo "restored ${source}"
-echo "Press a key"
-nohup "${pg_ctl}" -D "${ram_db}" start &
