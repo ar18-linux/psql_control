601b35e095579d98961d5c79c3c8bb325eafd275
nulysses <adr.zivil@gmail.com>
2021-04-28 21:50:40 +0200

    reformed, untested

--- psql_control/start_ram_db.sh
+++ psql_control/start_ram_db.sh
@@ -1,25 +1,59 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-psql_dir="$(cat "${DIR}/vars/psql_dir")"
-ram_db_dir="$(cat "${DIR}/vars/ram_db_dir")"
-_7za="$(cat "${DIR}/vars/7z_exec")"
+set -e
+set -x
 
-str="$(grep '[[:alnum:]]' "${DIR}/dbs.txt" | tail -n 1 | xargs)"
-stringarray=($str)
-source="${stringarray[0]}"
-port="${stringarray[1]}"
-sub_dir="default"
-pg_ctl="${psql_dir}/bin/pg_ctl"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-if [ -n "$port" ]; then
-  sub_dir="${port}"
-fi
 
-ram_db="${ram_db_dir}/${sub_dir}"
-echo "start ${str}?"
-#echo -ne '\n' | nohup "${pg_ctl}" -D "${ram_db}" start & >/dev/null 2>&1 &
-nohup "${pg_ctl}" -D "${ram_db}" start &
-#echo "${pg_ctl} -D ${ram_db} start &" | at now
+function init_vars(){
+  echo init_vars
+  . "${script_dir}/vars"
+}
 
-read -p "Press a key"
+
+function sanity_check(){
+  echo sanity_check
+  if [[ "$(whoami)" != "root" ]]; then
+    read -p "not root"
+    exit 1
+  fi
+  echo "_7za: ${_7za}"
+  echo "psql_dir: ${psql_dir}"
+  echo "ram_db_dir: ${ram_db_dir}"
+  echo "target: ${target}"
+  echo "db_user: ${db_user}"
+}
+
+
+function start(){
+  echo start
+  str="$(grep '[[:alnum:]]' "${script_dir}/dbs.txt" | tail -n 1 | xargs)"
+  stringarray=($str)
+  source="${stringarray[0]}"
+  port="${stringarray[1]}"
+  sub_dir="default"
+  pg_ctl="${psql_dir}/bin/pg_ctl"
+  
+  if [ -n "$port" ]; then
+    sub_dir="${port}"
+  fi
+  
+  ram_db="${ram_db_dir}/${sub_dir}"
+  echo "start ${str}?"
+  #echo -ne '' | nohup "${pg_ctl}" -D "${ram_db}" start & >/dev/null 2>&1 &
+  nohup "${pg_ctl}" -D "${ram_db}" start &
+  #echo "${pg_ctl} -D ${ram_db} start &" | at now
+  
+  read -p "Press a key"
+}
+
+
+function run(){
+  init_vars
+  sanity_check
+  restore
+}
+
+
+run
--- psql_control/stop_ram_db.sh
+++ psql_control/stop_ram_db.sh
@@ -1,40 +1,74 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-psql_dir="$(cat "${DIR}/vars/psql_dir")"
-ram_db_dir="$(cat "${DIR}/vars/ram_db_dir")"
-_7za="$(cat "${DIR}/vars/7z_exec")"
+set -e
+set -x
 
-pg_ctl="${psql_dir}/bin/pg_ctl"
-init_db="${psql_dir}/bin/initdb"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-#content="$(cat "${DIR}/dbs.txt" | xargs)"
-#echo "g:${content}"
-str="$(grep '[[:alnum:]]' "${DIR}/dbs.txt" | tail -n 1 | xargs)"
-#str="$(sed -e '/^[<blank><tab>]*$/d' "${DIR}/dbs.txt" | sed -n -e '$p')"
-#str="$(awk 'NF{p=$0}END{print p}' "${DIR}/dbs.txt")"
-#echo "d${str}d"
-#str="$(echo "${str}" | sed -e 's/^[ \t]*//')"
-#echo "r${str}r"
 
-stringarray=($str)
-source="${stringarray[0]}"
-port="${stringarray[1]}"
-sub_dir="default"
-#echo "w${port}w"
-if [ -n "$port" ]; then
-  sub_dir="$(echo "${port}" | xargs)"
-fi
+function init_vars(){
+  echo init_vars
+  . "${script_dir}/vars"
+}
 
-ram_db="${ram_db_dir}/${sub_dir}"
 
-#echo "w${str}w"
-#echo "w${ram_db}w"
-#echo "w${sub_dir}w"
-#printf '%s\n' "${stringarray[@]}"
-#echo "${pg_ctl} -D ${ram_db} stop -m f"
-read -p "stop ${str}?"
+function sanity_check(){
+  echo sanity_check
+  if [[ "$(whoami)" != "root" ]]; then
+    read -p "not root"
+    exit 1
+  fi
+  echo "_7za: ${_7za}"
+  echo "psql_dir: ${psql_dir}"
+  echo "ram_db_dir: ${ram_db_dir}"
+  echo "target: ${target}"
+  echo "db_user: ${db_user}"
+}
 
-"${pg_ctl}" -D "${ram_db}" stop -m f
 
-read -p "Press a key"
+function stop(){
+  echo stop
+  pg_ctl="${psql_dir}/bin/pg_ctl"
+  init_db="${psql_dir}/bin/initdb"
+  
+  #content="$(cat "${DIR}/dbs.txt" | xargs)"
+  #echo "g:${content}"
+  str="$(grep '[[:alnum:]]' "${DIR}/dbs.txt" | tail -n 1 | xargs)"
+  #str="$(sed -e '/^[<blank><tab>]*$/d' "${DIR}/dbs.txt" | sed -n -e '$p')"
+  #str="$(awk 'NF{p=$0}END{print p}' "${DIR}/dbs.txt")"
+  #echo "d${str}d"
+  #str="$(echo "${str}" | sed -e 's/^[ 	]*//')"
+  #echo "r${str}r"
+  
+  stringarray=($str)
+  source="${stringarray[0]}"
+  port="${stringarray[1]}"
+  sub_dir="default"
+  #echo "w${port}w"
+  if [ -n "$port" ]; then
+    sub_dir="$(echo "${port}" | xargs)"
+  fi
+  
+  ram_db="${ram_db_dir}/${sub_dir}"
+  
+  #echo "w${str}w"
+  #echo "w${ram_db}w"
+  #echo "w${sub_dir}w"
+  #printf '%s' "${stringarray[@]}"
+  #echo "${pg_ctl} -D ${ram_db} stop -m f"
+  read -p "stop ${str}?"
+  
+  "${pg_ctl}" -D "${ram_db}" stop -m f
+  
+  read -p "Press a key"
+}
+
+
+function run(){
+  init_vars
+  sanity_check
+  restore
+}
+
+
+run
