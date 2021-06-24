49ee1068ced7d525a228e89e09561a4efb04cca2
aremus <ar@d10.net>
2021-06-24 21:44:45 +0200

    1

--- psql_control/init.sh
+++ psql_control/init.sh
@@ -29,9 +29,12 @@ function prepare(){
   port="$(read_port_part "${str}")"
   pg_ver="$(read_version_part "${str}")"
   pg_ctl="$(fetch_psql_dir "${pg_ver}")/bin/pg_ctl"
+  my_psql="$(fetch_psql_dir "${pg_ver}")/bin/psql"
   init_db="$(fetch_psql_dir "${pg_ver}")/bin/initdb"
   
-  read -p "init ${str}? ALL CURRENT DATA WILL BE LOST!!!"
+  if [ "${ask_for_confirmation}" = "1" ]; then
+    read -p "init ${str}? ALL CURRENT DATA WILL BE LOST!!!"
+  fi
   
   if [[ -d "${source}" ]]; then
     set +e
@@ -46,13 +49,22 @@ function prepare(){
   chown "${db_user}" "${source}"
   
   su - "${db_user}" -c "${init_db} -D ${source} -E utf-8"
+  sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" "${source}/postgresql.conf"
   sed -i "s/#port = 5432/port = ${port}/g" "${source}/postgresql.conf"
+  su - "${db_user}" -c "echo \"host  all  all 0.0.0.0/0 md5\" >> ${source}/pg_hba.conf"
+  
   su - "${db_user}" -c "${pg_ctl} -D ${source} start"
+  su - "${db_user}" -c "${my_psql} -p ${port} -d postgres -c \"ALTER USER ${db_user} PASSWORD 'postgres';\""
 }
 
 
 function run(){
   db_name="$1"
+  ask_for_confirmation="${2}"
+  if [ "${ask_for_confirmation}" = "" ]; then
+    ask_for_confirmation="1"
+  fi
+  export ask_for_confirmation="${ask_for_confirmation}"
   init_vars
   sanity_check
   prepare
